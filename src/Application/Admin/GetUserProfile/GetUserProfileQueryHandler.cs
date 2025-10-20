902f93c2dd110589c4de3fcd47126a85e389d9ef
JCDWHIZ <jesseugboh@gmail.com>
2025-10-20 16:01:59 +0100

    added get user profile

---
+++ src/Application/Admin/GetUserProfile/GetUserProfileQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Abstractions.Authentication;
+
+namespace Application.Admin.GetUserProfile;
+public class GetUserProfileQueryHandler(IApplicationDbContext _context, IUserContext userContext) : IQueryHandler<GetUserProfileQuery, UserDto>
+{
+    public async Task<Result<UserDto>> Handle(GetUserProfileQuery query, CancellationToken cancellationToken)
+    {
+        UserDto? user = await _context.Users
+            .Where(u => u.PublicId == userContext.UserPublicId)
+            .Select(u => new UserDto
+            {
+                PublicId = u.PublicId,
+                Username = u.Username,
+                Email = u.Email,
+                PhoneNumber = u.PhoneNumber,
+                ProfilePic = u.ProfilePic
+            })
+            .FirstOrDefaultAsync(cancellationToken);
+        if (user is null)
+        {
+            return Result.Failure<UserDto>(UserErrors.NotFound(query.PublicId));
+        }
+        return Result.Success(user);
+    }
+}
---
+++ src/Web.Api/Endpoints/Admin/GetUserProfile.cs
@@ -0,0 +1,24 @@
+﻿using Application.Admin;
+using Application.Admin.GetUserProfile;
+using Web.Api.Extensions;
+
+namespace Web.Api.Endpoints.Admin;
+
+internal sealed class GetUserProfileEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/admin/profile", async (
+            IQueryHandler<GetUserProfileQuery, UserDto> handler,
+            CancellationToken cancellationToken) =>
+        {
+            var query = new GetUserProfileQuery();
+
+            Result<UserDto> result = await handler.Handle(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .WithTags(Tags.Admin)
+        .RequireAuthorization();
+    }
+}
